7cc626afa52acd48876558d228d49a14fbe80432
Axel Tifrani <axeltifrani@gmail.com>
2026-02-14 10:47:00 +0100

    add button

--- myapp.py
+++ myapp.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.write('HelloMBADIA !')
+st.header('Exemple : st.button')
+
+if st.button('say hello'):
+     
+     st.write('why hello?')
+else:
+     st.write('goodbye')
